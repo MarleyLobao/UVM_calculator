885d229d0dc081c7381f736fb0ae0f0a8c7d7538
Hugo Gayoso <gayosohugo@gmail.com>
2022-03-24 16:52:25 -0300

    Add interface;

---
+++ tb/src/calculator_io.sv
@@ -0,0 +1,30 @@
+/*
+Author: Hugo G.
+Description: This file models a calculator RTL interface
+*/
+
+interface calculator_io(
+  input clk, rst_n
+);
+  logic [7:0] operand_A;
+  logic [7:0] operand_B;
+  logic [7:0] op_code;
+  logic [7:0] result;
+
+  modport dut(input operand_A,
+              input operand_B,
+              input op_code,
+              input clk,
+              input rst_n,
+              output data);
+
+  modport tb(output operand_A,
+              output operand_B,
+              output op_code,
+              output clk,
+              output rst_n,
+              input data);
+
+endinterface : calculator_io
+
+
