263736a056969c049f38c824872da460fceff07d
Hugo Gayoso <gayosohugo@gmail.com>
2022-03-25 14:21:39 -0300

    Update calculator_io.sv

--- tb/src/calculator_io.sv
+++ tb/src/calculator_io.sv
@@ -6,24 +6,24 @@ Description: This file models a calculator RTL interface
 interface calculator_io(
   input clk, rst_n
 );
-  logic [7:0] operand_A;
+  logic [7:0] dat_a_in;
   logic [7:0] operand_B;
-  logic [1:0] op_code;
-  logic [15:0] result;
+  logic [1:0] function_in;
+  logic [15:0] out;
 
-  modport dut(input operand_A,
-              input operand_B,
-              input op_code,
+  modport dut(input dat_a_in,
+              input dat_b_in,
+              input function_in,
               input clk,
               input rst_n,
-              output data);
+              output out);
 
-  modport tb(output operand_A,
-              output operand_B,
-              output op_code,
-              output clk,
-              output rst_n,
-              input data);
+  modport tb(output dat_a_in,
+              output dat_b_in,
+              output function_in,
+              input clk,
+              input rst_n,
+              input out);
 
 endinterface : calculator_io
 
