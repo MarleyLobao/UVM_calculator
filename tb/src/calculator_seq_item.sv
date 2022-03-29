26fbadc5cef0a2dafd3858c83214440bcaff994e
MarleyLobao <marley.sousa@ee.ufcg.edu.br>
2022-03-29 10:33:54 -0300

    adding my transaction file

--- tb/src/calculator_seq_item.sv
+++ tb/src/calculator_seq_item.sv
@@ -1,19 +1,13 @@
-/*
- Author: Hugo G.
- Description: This files defines a basic sequence item 
-*/
-
 class calculator_seq_item extends uvm_sequence_item;
-  rand bit [7:0] m_dat_a_in;
-  rand bit [7:0] m_dat_b_in;
-  rand bit [1:0] m_function_in;
-       bit [15:0] m_out;
+  `uvm_object_utils(calculator_seq_item)
 
-  `uvm_object_utils_begin(computation)
-      `uvm_field_int(m_dat_a_in, UVM_ALL_ON|UVM_HEX)
-      `uvm_field_int(m_dat_b_in, UVM_ALL_ON|UVM_HEX)
-      `uvm_field_int(m_function_in, UVM_ALL_ON|UVM_HEX)
-      `uvm_field_int(m_out, UVM_ALL_ON|UVM_HEX)
-  `uvm_object_utils_end
+  rand bit [1:0]  function_in;
+  rand bit [7:0]  dat_a_in;
+  rand bit [7:0]  dat_b_in;
+  bit [15:0] out;
 
-endclass : calculator_seq_item
+  function new(string name = "");
+    super.new(name);
+  endfunction
+
+endclass
