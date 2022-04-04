d065bf597e596b8c44aca5fa9d2cd8c46c19e11f
MarleyLobao <marley.sousa@ee.ufcg.edu.br>
2022-04-04 17:34:08 -0300

    adding monitor and one analysis port to the agent

--- tb/src/calculator_agent.sv
+++ tb/src/calculator_agent.sv
@@ -7,11 +7,13 @@ class calculator_agent extends uvm_agent;
     calculator_driver    drv;
     calculator_monitor   mon;
 
-    uvm_analysis_port #(calculator_seq_item) agent_port;
+    uvm_analysis_port #(calculator_seq_item) agent_port_in;
+    uvm_analysis_port #(calculator_seq_item) agent_port_out;
 
     function new(string name = "calculator_agent", uvm_component parent = null);
         super.new(name, parent);
-        agent_port = new("agent_port", this);
+        agent_port_in = new("agent_port_in", this);
+        agent_port_out = new("agent_port_out", this);
     endfunction
 
     virtual function void build_phase(uvm_phase phase);
@@ -23,7 +25,8 @@ class calculator_agent extends uvm_agent;
 
     virtual function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-        mon.agent_port.connect(agent_port);
+        mon.monitor_port_in.connect(agent_port_in);
+        mon.monitor_port_out.connect(agent_port_out);
         drv.seq_item_port.connect(sqr.seq_item_export);
     endfunction
 endclass: calculator_agent
---
+++ tb/src/calculator_monitor.sv
@@ -0,0 +1,56 @@
+class calculator_monitor extends uvm_monitor;
+    `uvm_component_utils(calculator_monitor)
+
+    virtual calculator_if.mst inter;
+    calculator_seq_item seq_item_in, seq_item_out;
+
+    uvm_analysis_port #(calculator_seq_item) monitor_port_in;
+    uvm_analysis_port #(calculator_seq_item) monitor_port_out;
+
+    function new(string name = "calculator_monitor", uvm_component parent = null);
+        super.new(name, parent);
+        monitor_port_in = new("monitor_port_in",this);
+        monitor_port_out = new("monitor_port_out",this);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        seq_item_in = calculator_seq_item::type_id::create("seq_item_in",this);
+        seq_item_out = calculator_seq_item::type_id::create("seq_item_out",this);
+        
+        if(!uvm_config_db#(virtual calculator_if.mst)::get(this, "", "inter", inter)) begin
+            `uvm_fatal("NOVIF","The virtual connection wasn't successful!");
+        end
+
+    endfunction
+
+    virtual task run_phase(uvm_phase phase);
+        super.run_phase(phase);
+        
+        wait(rst_n === 0);
+        
+        @(posedge inter.rst_n);
+        fork
+
+            forever begin
+                @(posedge inter.clk)
+                seq_item_in.dat_a_in <= inter.dat_a_in;
+                seq_item_in.dat_b_in <= inter.dat_b_in;
+                seq_item_in.function_in <= inter.function_in;
+                monitor_port_in.write(seq_item_in);
+            end
+
+            begin
+                @(posedge inter.clk)
+
+                forever begin
+                    @(posedge inter.clk)
+                    seq_item_out.out <= inter.out;
+                    monitor_port_out.write(seq_item_out);
+                end
+            end
+        join           
+
+
+    endtask
+endclass : calculator_monitor
